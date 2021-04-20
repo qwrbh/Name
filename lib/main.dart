1d9226163e2bd9c337784c0497d0c4bfb833260b
sashasoly <sashasoly@bk.ru>
2021-04-20 19:10:50 +0300

    Начальный коммит

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Тест',
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Задание от Максима')),
+        body: Picture(),
+      ),
+    );
+  }
+}
+
+class Picture extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        child: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
